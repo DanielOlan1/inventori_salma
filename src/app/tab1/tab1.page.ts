ccb7febed34194ff0b0ed498b35c3229fbb01e5f
DanielOlan1 <97993904+DanielOlan1@users.noreply.github.com>
2023-11-10 03:19:40 -0600

    asd

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { ModalController, AlertController } from '@ionic/angular';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -10,9 +9,22 @@ import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 export class Tab1Page {
   productName: string = '';
   quantity: number = 0;
-  inventoryData: { productName: string, quantity: number, photoDataUrl: string }[] = [];
+  inventoryData: { productName: string, quantity: number }[] = [];
+  productOptions: string[] = [
+  'GPS TRACTO', 
+  'GPS TANQUES', 
+  'SENSOR COMBUSTIBLE',
+  'SENSOR PUERTA OPERADOR',
+  'SENSOR PUERTA CHOFER',
+  'SENSOR BUZZER',
+  'SENSOR SIRENA',
+  'PARO DE MOTOR',
+  'CAMARA OPERADOR',
+  'CAMARA CARRETERA',
+  'TABLETA',
+  'BARRAS MAGNETICAS'];
 
-  constructor(private modalController: ModalController, private alertController: AlertController, private camera: Camera) {}
+  constructor(private modalController: ModalController, private alertController: AlertController, ) {}
 
   ngOnInit() {
     const savedData = localStorage.getItem('inventory');
@@ -21,42 +33,22 @@ export class Tab1Page {
     }
   }
 
-  async tomarFoto() {
-    try {
-      const options: CameraOptions = {
-        quality: 100,
-        destinationType: this.camera.DestinationType.DATA_URL,
-        encodingType: this.camera.EncodingType.JPEG,
-        mediaType: this.camera.MediaType.PICTURE
-      };
-
-      const imageData = await this.camera.getPicture(options);
-
-      if (imageData) {
-        // Validar los datos antes de agregarlos al Local Storage
-        if (this.productName.trim() === '' || this.quantity <= 0) {
-          return;
-        }
-
-        // Agregar el nuevo producto con la foto
-        this.inventoryData.push({
-          productName: this.productName,
-          quantity: this.quantity,
-          photoDataUrl: 'data:image/jpeg;base64,' + imageData,
-        });
-
-        // Guardar la información actualizada en el Local Storage
-        localStorage.setItem('inventory', JSON.stringify(this.inventoryData));
-
-        // Limpiar los campos del formulario
-        this.productName = '';
-        this.quantity = 0;
-      }
-    } catch (error) {
-      console.error('Error al tomar la foto', error);
+  agregarProducto() {
+    // Validar los datos antes de agregarlos al Local Storage
+    if (this.productName.trim() === '' || this.quantity <= 0) {
+      return;
     }
-  }
 
+    // Agregar el nuevo producto
+    this.inventoryData.push({ productName: this.productName, quantity: this.quantity });
+
+    // Guardar la información actualizada en el Local Storage
+    localStorage.setItem('inventory', JSON.stringify(this.inventoryData));
+
+    // Limpiar los campos del formulario
+    this.productName = '';
+    this.quantity = 0;
+  }
 
   async eliminarProducto(index: number) {
     const confirmDelete = await this.alertController.create({
